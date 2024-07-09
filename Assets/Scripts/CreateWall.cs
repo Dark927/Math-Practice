bd2b9232312dacd1f843e73d221b1e8d58b5ec34
Dark PC <malush1981@gmail.com>
2024-07-09 19:24:08 +0300

    complete challenge
    make ball stop when it intersects with wall.

--- Assets/Scripts/CreateWall.cs
+++ Assets/Scripts/CreateWall.cs
@@ -7,6 +7,7 @@ public class CreateWall : MonoBehaviour
     Line wall;
     Line ballPath;
     public GameObject ball;
+    private Line _trajectory;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,22 @@ public class CreateWall : MonoBehaviour
 
         ballPath = new Line(new Coords(-6, 0, 0), new Coords(100, 0, 0));
         ballPath.Draw(0.1f, Color.yellow);
+
+        ball.transform.position = ballPath.Lerp(0).ToVector();
+
+
+        float _intersectionT = ballPath.IntersectsAt(wall);
+        float _intersectionS = wall.IntersectsAt(ballPath);
+
+        if (!float.IsNaN(_intersectionT) && !float.IsNaN(_intersectionS))
+        {
+            _trajectory = new Line(ballPath.A, ballPath.Lerp(_intersectionT), Line.LINETYPE.SEGMENT);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ball.transform.position = _trajectory.Lerp(Time.time * 0.1f).ToVector();
     }
 }
