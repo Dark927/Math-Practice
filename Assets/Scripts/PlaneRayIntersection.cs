96882db48461a7da5df8776f463e527aa12a444c
Dark PC <malush1981@gmail.com>
2024-07-10 15:47:54 +0300

    import sheep model, complete the course challenge.
    set the plane horizontally and add a sheep to it.
    move the sheep on the plane when holding the left mouse button.
    check boundaries for the plane and do not move the sheep beyond them,
    use simple coordinates for setting these boundaries.

--- Assets/Scripts/PlaneRayIntersection.cs
+++ Assets/Scripts/PlaneRayIntersection.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class PlaneRayIntersection : MonoBehaviour
 {
-    [SerializeField] private GameObject _sphere;
+    [SerializeField] private GameObject _sheep;
+    [SerializeField] private Vector3 _offset = new Vector3(0, 0.3f, 0);
+
+    [SerializeField] private Vector2 _boundsX = new Vector3(-7f, 7f);
+    [SerializeField] private Vector2 _boundsZ = new Vector3(-7f, 7f);
 
     Plane _plane;
 
@@ -11,28 +15,34 @@ public class PlaneRayIntersection : MonoBehaviour
         Vector3[] vertices = GetComponent<MeshFilter>().mesh.vertices;
 
         _plane = new Plane(
-            transform.TransformPoint(vertices[0]),
-            transform.TransformPoint(vertices[1]),
-            transform.TransformPoint(vertices[2]));
+            transform.TransformPoint(vertices[0]) + _offset,
+            transform.TransformPoint(vertices[1]) + _offset,
+            transform.TransformPoint(vertices[2]) + _offset);
     }
 
     private void Update()
     {
-        // Move sphere while holding mouse
+        DragSheepLogic();
+    }
 
-        if(Input.GetMouseButton(0))
+    private void DragSheepLogic()
+    {
+        if (Input.GetMouseButton(0))
         {
-            // Cast ray from mouse position 
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             float t = 0f;
 
-            // Check intersection of ray with plane and move sphere
-
-            if(_plane.Raycast(ray, out t))
+            if (_plane.Raycast(ray, out t))
             {
-                Vector3 hitPoint = ray.GetPoint(t);
-                _sphere.transform.position = hitPoint;
+                Vector3 intersectionPoint = ray.GetPoint(t);
+
+                bool outOfBoundsX = (intersectionPoint.x < _boundsX.x) || (_boundsX.y < intersectionPoint.x);
+                bool outOfBoundsZ = (intersectionPoint.z < _boundsZ.x) || (_boundsZ.y < intersectionPoint.z);
+
+                if (!(outOfBoundsX || outOfBoundsZ))
+                {
+                    _sheep.transform.position = intersectionPoint;
+                }
             }
         }
     }
